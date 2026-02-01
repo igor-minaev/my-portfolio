be237821fa88a35f083c41d71df43256534efe6c
Igor Minayeu <igor.minayeu@gmail.com>
2026-02-01 14:43:58 +0300

    add Main component

---
+++ src/layout/sections/main/Main.tsx
@@ -0,0 +1,16 @@
+import photo from "../../../assets/images/photo.jpg"
+
+export const Main = () => {
+    return (
+        <div>
+            <div>
+                <p>Hi 👋,</p>
+                <p>My name is</p>
+                <h2>Pavan MG</h2>
+                <h1> I build things for web</h1>
+            </div>
+            <img src={photo} alt=""/>
+        </div>
+    );
+};
+
