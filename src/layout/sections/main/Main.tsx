111eb423c5ca040a5a950d4e42e33803cf5931f5
Igor Minayeu <igor.minayeu@gmail.com>
2026-02-01 15:06:47 +0300

    refactor Main component

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -1,16 +1,37 @@
 import photo from "../../../assets/images/photo.jpg"
+import styled from "styled-components";
 
 export const Main = () => {
     return (
-        <div>
+        <StyledMain>
             <div>
                 <p>Hi 👋,</p>
                 <p>My name is</p>
-                <h2>Pavan MG</h2>
-                <h1> I build things for web</h1>
+                <Name>Pavan MG</Name>
+                <MainTitle> I build things for web</MainTitle>
             </div>
-            <img src={photo} alt=""/>
-        </div>
+            <Photo src={photo} alt=""/>
+        </StyledMain>
     );
 };
+
+const StyledMain = styled.div`
+    background-color: rgb(253, 170, 133);
+    min-height: 100vh;
+`
+const Photo = styled.img`
+    width: 349px;
+    height: 349px;
+    border-radius: 50%;
+    object-fit: cover;
+    border: 9px solid rgba(70, 3, 170, 0.7);
+`
+const MainTitle = styled.h1`
+
+`
+
+const Name = styled.h2`
+
+`
+
 
