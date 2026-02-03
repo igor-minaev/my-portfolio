3b63e9ccb9aff307475485d80dca610b9f3d0dad
Igor Minayeu <igor.minayeu@gmail.com>
2026-02-03 17:23:43 +0300

    refactor Contacts component

--- src/layout/sections/contacts/Contacts.tsx
+++ src/layout/sections/contacts/Contacts.tsx
@@ -1,10 +1,20 @@
 import styled from "styled-components";
+import {SectionTitle} from "../../../components/SectionTitle.tsx";
+import {SectionDescription} from "../../../components/SectionDescription.tsx";
+import {Button} from "../../../components/Button.tsx";
 
 
 export const Contacts = () => {
     return (
         <StyledContacts>
-
+            <SectionTitle>Contacts</SectionTitle>
+            <SectionDescription>For any questions please mail me:</SectionDescription>
+            <StyledForm>
+                <Field placeholder="name"></Field>
+                <Field placeholder="subject"></Field>
+                <Field placeholder="message" as="textarea"></Field>
+                <Button type="submit">Send message</Button>
+            </StyledForm>
         </StyledContacts>
     );
 };
@@ -13,3 +23,17 @@ const StyledContacts = styled.div`
     min-height: 50vh;
     background-color: #d5ff6c;
 `
+
+const StyledForm = styled.form`
+    max-width: 400px;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    margin: 0 auto;
+`
+const Field = styled.input`
+
+`
+
+
