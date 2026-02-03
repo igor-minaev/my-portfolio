5c9fdc6633b784b1f88565aa194c74f4f1a2a4b1
Igor Minayeu <igor.minayeu@gmail.com>
2026-02-03 21:28:38 +0300

    add Footer component

---
+++ src/layout/footer/Footer.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+
+export const Footer = () => {
+    return (
+        <StyledFooter>
+
+        </StyledFooter>
+    );
+};
+
+const StyledFooter = styled.footer`
+
+`
+
