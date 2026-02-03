0107a53b0ecfcc29562a995d004446e12729183a
Igor Minayeu <igor.minayeu@gmail.com>
2026-02-03 17:13:56 +0300

    add Contacts component

---
+++ src/layout/sections/contacts/Contacts.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+
+export const Contacts = () => {
+    return (
+        <StyledContacts>
+
+        </StyledContacts>
+    );
+};
+
+const StyledContacts = styled.div`
+    min-height: 50vh;
+    background-color: #d5ff6c;
+`
