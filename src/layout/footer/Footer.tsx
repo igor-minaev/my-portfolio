7b032c7c4d966ee17c0edd9155f63ff5cac98146
Igor Minayeu <igor.minayeu@gmail.com>
2026-02-04 20:19:38 +0300

    add FlexWrapper

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -1,15 +1,31 @@
 import styled from "styled-components";
+import {Logo} from "../../components/logo/Logo.tsx";
+import {Social} from "../../components/social/Social.tsx";
+import {FlexWrapper} from "../../components/FlexWrapper.tsx";
 
 
 export const Footer = () => {
     return (
         <StyledFooter>
-
+            <FlexWrapper  justify="space-between" alignItem="center">
+                <Logo/>
+                <PhoneNumber> +3445762358252</PhoneNumber>
+                <Email>someemail@gmail.com</Email>
+                <Social/>
+            </FlexWrapper>
         </StyledFooter>
     );
 };
 
 const StyledFooter = styled.footer`
+    background-color: #f893bf;
+    min-height: 20vh;
+`
 
+const PhoneNumber = styled.span`
+   
+`
+const Email = styled.span`
+   
 `
 
--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -2,20 +2,21 @@ import styled from "styled-components";
 import {Logo} from "../../components/logo/Logo.tsx";
 import {Menu} from "../../components/menu/Menu.tsx";
 import {Social} from "../../components/social/Social.tsx";
+import {FlexWrapper} from "../../components/FlexWrapper.tsx";
 
 export const Header = () => {
     return (
         <StyledHeader>
-            <Logo/>
-            <Menu/>
-            <Social/>
+            <FlexWrapper justify="space-between">
+                <Logo/>
+                <Menu/>
+                <Social/>
+            </FlexWrapper>
         </StyledHeader>
     );
 };
 
 const StyledHeader = styled.header`
     background-color: rgb(255, 253, 129);
-    display: flex;
-    justify-content: space-between;
 `
 
