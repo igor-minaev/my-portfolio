75dd25842116e8cfd9ec28992966c4dc664b178f
Igor Minayeu <igor.minayeu@gmail.com>
2026-02-15 16:27:53 +0300

    add styles for Footer component

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -1,31 +1,52 @@
 import styled from "styled-components";
 import {Logo} from "../../components/logo/Logo.tsx";
- // import {Social} from "../../components/social/Social.tsx";
 import {FlexWrapper} from "../../components/FlexWrapper.tsx";
+import {Container} from "../../components/Container.tsx";
+import {theme} from "../../styles/Theme.ts";
 
 
 export const Footer = () => {
     return (
         <StyledFooter>
-            <FlexWrapper justify="space-between" alignItem="center">
-                <Logo/>
-                <PhoneNumber href="tel:+3445762358252"> +3445762358252</PhoneNumber>
-                <Email href="mailto:someemail@gmail.com">someemail@gmail.com</Email>
-                {/*<Social/>*/}
-            </FlexWrapper>
+            <Container>
+                <FlexWrapper justify="space-between" alignItem="center">
+                    <Logo iconId="logo"/>
+                    <ConnectionWrapper>
+                        <PhoneNumber href="tel:+3445762358252"> +3445762358252</PhoneNumber>
+                        <Email href="mailto:someemail@gmail.com">someemail@gmail.com</Email>
+                    </ConnectionWrapper>
+                </FlexWrapper>
+            </Container>
         </StyledFooter>
     );
 };
 
 const StyledFooter = styled.footer`
-    background-color: #f893bf;
-    min-height: 20vh;
+    background-color: ${theme.colors.background};
+`
+
+const ConnectionWrapper = styled.footer`
+    display: flex;
+    gap: 50px;
+    font-family: "DM Sans", sans-serif;
+    font-weight: 400;
+    font-size: 18px;
 `
 
 const PhoneNumber = styled.a`
-
+    color: ${theme.colors.secondaryFont};
+    &:hover{
+        background: ${theme.gradients.primaryFont};
+        background-clip: text;
+        color: transparent;
+    }
 `
 const Email = styled.a`
-
+    color: ${theme.colors.secondaryFont};
+    &:hover{
+        background: ${theme.gradients.primaryFont};
+        background-clip: text;
+        color: transparent;
+    }
 `
 
