66b86642f540770020fd908e289c3e609df00d80
Igor Minayeu <igor.minayeu@gmail.com>
2026-02-15 16:28:47 +0300

    refactor

--- src/components/social/Social.tsx
+++ src/components/social/Social.tsx
@@ -1,13 +1,24 @@
 import {Icon} from "../icon/Icon.tsx";
 import styled from "styled-components";
-import type {SocialItem} from "../../layout/header/Header.tsx";
 import {theme} from "../../styles/Theme.ts";
 
+type SocialItem = {
+    id: string
+    iconId: string
+    width: string
+    height: string
+    viewBox: string
+}
+const socialIcons: Array<SocialItem> = [
+    {id: crypto.randomUUID(), iconId: "github", width: "30", height: "30", viewBox: "0 0 30 30"},
+    {id: crypto.randomUUID(), iconId: "twitter", width: "30", height: "30", viewBox: "0 0 30 30"},
+    {id: crypto.randomUUID(), iconId: "linkedin", width: "30", height: "30", viewBox: "0 0 30 30"}
+]
 
-export const Social = (props: { socialItems: Array<SocialItem> }) => {
+export const Social = () => {
     return (
         <StyledSocial>
-            {props.socialItems.map((item) => (
+            {socialIcons.map((item) => (
                 <ListItem key={item.id}>
                     <Link href="#">
                         <Icon iconId={item.iconId} width={item.width} height={item.height} viewBox={item.viewBox}/>
@@ -22,7 +33,6 @@ const StyledSocial = styled.ul`
     display: flex;
     gap: 20px;
     list-style-type: none;
-    padding-left: 40px;
 
 `
 const ListItem = styled.li`
@@ -32,13 +42,14 @@ const Link = styled.a`
     display: flex;
     align-items: center;
     color: ${theme.colors.secondaryFont};
+
     svg {
-        
+
     }
 
     &:hover {
         color: ${theme.colors.tertiaryFont};
-        transform: translateY(-4px) ;
+        transform: translateY(-4px);
     }
 `
 
