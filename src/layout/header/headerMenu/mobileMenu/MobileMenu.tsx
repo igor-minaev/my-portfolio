889b0ba8a82e1fab6f83ee4af4c6caf855950fdb
Igor Minayeu <igor.minayeu@gmail.com>
2026-02-25 18:46:16 +0300

    add functions for mobile menu

--- src/layout/header/headerMenu/mobileMenu/MobileMenu.tsx
+++ src/layout/header/headerMenu/mobileMenu/MobileMenu.tsx
@@ -1,7 +1,7 @@
 import {Social} from "../../../../components/social/Social.tsx";
 import {Menu} from "../menu/Menu.tsx";
 import type {SocialItem} from "../../Header.tsx";
-import React from "react";
+import React, {useState} from "react";
 import {S} from './../HeaderMenu_Styles.ts'
 
 
@@ -9,12 +9,14 @@ export const MobileMenu: React.FC<{ menuItems: Array<string>, socialIcons: Array
     menuItems: Array<string>,
     socialIcons: Array<SocialItem>
 }) => {
+    const [menuIsOpen, setMenuIsOpen] = useState(false)
+    const onBurgerButtonClick = () => setMenuIsOpen(!menuIsOpen)
     return (
         <S.MobileMenu>
-            <S.BurgerButton isOpen={false}>
+            <S.BurgerButton isOpen={menuIsOpen} onClick={onBurgerButtonClick}>
                 <span></span>
             </S.BurgerButton>
-            <S.MobileMenuPopup isOpen={false}>
+            <S.MobileMenuPopup isOpen={menuIsOpen} onClick={() => setMenuIsOpen(!menuIsOpen)}>
                 <Menu menuItems={props.menuItems}/>
                 <Social socialIcons={props.socialIcons}/>
             </S.MobileMenuPopup>
